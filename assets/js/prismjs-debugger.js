cd7aac562770d6d22242990360f1eb8fa700f9ab
pjehan <pierre.jehan@gmail.com>
2018-10-29 14:23:08 +0100

    Add new slides

---
+++ assets/js/prismjs-debugger.js
@@ -0,0 +1,79 @@
+const prismjsDebugger = {
+
+  debuggerContainer: null,
+  consoleContainer: null,
+  buttons: {},
+  lines: [],
+  console: false,
+  lineHeight: 0,
+  currentLine: 0,
+  pointer: null,
+
+  initialize: function(el, lines, console) {
+    this.debuggerContainer = el;
+    this.lines = lines;
+    this.console = console;
+    this.lineHeight = parseFloat(window.getComputedStyle(this.debuggerContainer).getPropertyValue('line-height'));
+
+    this.createDebugger();
+    if (this.console === true) {
+      this.createConsole();
+    }
+
+    return this;
+  },
+
+  createDebugger() {
+    const debuggerContainer = document.createElement('div');
+    debuggerContainer.classList.add('prismjs-debugger');
+
+    this.buttons.gotToStartButton = document.createElement('button');
+    this.buttons.gotToStartButton.textContent = '⏮';
+    this.buttons.gotToStartButton.addEventListener('click', this.goToStart.bind(this));
+    debuggerContainer.appendChild(this.buttons.gotToStartButton);
+
+    this.buttons.playButton = document.createElement('button');
+    this.buttons.playButton.textContent = '⏯';
+    this.buttons.playButton.addEventListener('click', this.play.bind(this));
+    debuggerContainer.appendChild(this.buttons.playButton);
+
+    this.debuggerContainer.insertBefore(debuggerContainer, this.debuggerContainer.childNodes[0]);
+  },
+
+  createConsole() {
+    this.consoleContainer = document.createElement('div');
+    this.consoleContainer.classList.add('prismjs-console');
+    this.debuggerContainer.appendChild(this.consoleContainer);
+  },
+
+  goToStart: function() {
+    this.currentLine = 0;
+    this.consoleContainer.innerHTML = '';
+    this.buttons.playButton.disabled = false;
+    this.play();
+  },
+
+  play: function () {
+    const currentLine = this.lines[this.currentLine];
+    if (!this.pointer) {
+      this.pointer = document.createElement('div');
+      this.pointer.classList.add('prismjs-pointer');
+      this.pointer.textContent = '>';
+      this.debuggerContainer.appendChild(this.pointer);
+    }
+    if (this.currentLine <= this.lines.length - 1) {
+      if (this.console && currentLine.console) {
+        const consoleMsg = document.createElement('div');
+        consoleMsg.classList.add('prismjs-console-msg');
+        consoleMsg.textContent = currentLine.console;
+        this.consoleContainer.appendChild(consoleMsg);
+      }
+      this.pointer.style.top = (currentLine.number - 1) * this.lineHeight + 'px';
+      this.currentLine++;
+      if (this.currentLine >= this.lines.length) {
+        this.buttons.playButton.disabled = true;
+      }
+    }
+  }
+
+};
