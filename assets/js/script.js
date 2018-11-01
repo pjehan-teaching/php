6a3944059bd0ac596ba75d26a9ce423b46fe3760
pjehan <pierre.jehan@gmail.com>
2018-11-01 11:06:34 +0100

    Fix debugger and add function exercices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
   // TODO: Fix this hack within ReavealExpress so it would start this script after loading the HTML into the DOM
   setTimeout(function() {
 
-    prismjsDebugger.initialize(document.getElementById('while-loop'), [
+    var whileDebugger = prismjsDebugger(document.getElementById('while-loop'), [
       { number: 2 },
       { number: 4 },
       { number: 5, console: '3' },
@@ -16,16 +16,16 @@
       { number: 6 },
       { number: 4 },
       { number: 9, console: 'Bonne année !!' }
-    ], true);
+    ], true).initialize();
 
-    prismjsDebugger.initialize(document.getElementById('function-bonjour'), [
+    var functionDebugger = prismjsDebugger(document.getElementById('function-bonjour'), [
       { number: 1 },
       { number: 5 },
       { number: 1, console: '$pseudo = "John"' },
       { number: 2 },
       { number: 5, console: '$message = "Bonjour John"' },
       { number: 6, console: 'Bonjour John' }
-    ], true);
+    ], true).initialize();
 
   }, 500);
 
